0ec57886f3c3255fb92479eb413843e0c24daef0
Ilia Grigoriev <grigoriev.ilia1@yandex.ru>
2023-11-14 02:23:42 +0300

    added task3

---
+++ src/main/java/edu/hw5/task3/DashDateHandler.java
@@ -0,0 +1,22 @@
+package edu.hw5.task3;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+public class DashDateHandler extends DateHandler {
+
+    @Override
+    public Optional<LocalDate> parseDate(String string) {
+        String[] parts = string.split("-");
+        if (parts.length != 3) { // can't handle
+            if (next != null) {
+                return next.parseDate(string);
+            }
+            return Optional.empty();
+        }
+        int year = Integer.parseInt(parts[0]);
+        int month = Integer.parseInt(parts[1]);
+        int day = Integer.parseInt(parts[2]);
+        return Optional.of(LocalDate.of(year, month, day));
+    }
+}
